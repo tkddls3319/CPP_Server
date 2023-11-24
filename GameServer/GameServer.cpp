3076ab35d8a78e5f3d1cb528b517f93708e68d9b
SangInPark <54829486+tkddls3319@users.noreply.github.com>
2023-11-24 16:33:24 +0900

    2023.11.24
    스핀락, 슬립락 공부함.

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -1,26 +1,49 @@
 ﻿#include "pch.h"
 #include <iostream>
 #include <thread>
-#include <atomic>
-//atomic : All-or-Nothing(전부실행 하거나 하나도 실행하지 않게) db의 트렌젝션 개념
-atomic<int32> sum = 0;
-void Add() {
-	for (int32 i = 0; i < 100'0000; i++) 
+#include <mutex>
+
+class SpinLock {
+public:
+	void lock() 
 	{
-		sum.fetch_add(1);
+		bool expected = false;
+		bool desired = true;
+
+		while (_locked.compare_exchange_strong(expected, desired) == false)
+		{
+			expected = false;
+			//this_thread::sleep_for(std::chrono::milliseconds(100));//언제까지 멈춰라
+			this_thread::sleep_for(0ms);//입력한 시간동안 제 스케줄링되지않는다.
+			//this_thread::yield();////언제든지 스케줄링될 수 있지만 지금은 필요하지않으니 반환하겠다.
+		}
+	}
+	void unlock() 
+	{
+		_locked.store(false);
+	}
+private:
+	atomic<bool> _locked = false;
+};
+int32 sum = 0;
+mutex m;
+SpinLock spinLock;
+void Add() {
+	for (int32 i = 0; i < 10'0000; i++) 
+	{
+		lock_guard<SpinLock> gard(spinLock);
+		sum++;
 	}
 }
 void Sub() {
-	for (int32 i = 0; i < 100'0000; i++)
+	for (int32 i = 0; i < 10'0000; i++)
 	{
-		sum.fetch_add(-1);
+		lock_guard<SpinLock> gard(spinLock);
+		sum--;
 	}
 }
 int main()
 {
-	Add();
-	Sub();
-
 	std::thread t1(Add);
 	std::thread t2(Sub);
 
