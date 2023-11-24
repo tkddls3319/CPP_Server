aaa31fd3cfaf168706f34c7b87673fb3775ae3e0
SangInPark <54829486+tkddls3319@users.noreply.github.com>
2023-11-24 14:26:22 +0900

    2023.11.24
    프로젝트 기본

---
+++ GameServer/GameServer.cpp
@@ -0,0 +1,30 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <thread>
+#include <atomic>
+//atomic : All-or-Nothing(전부실행 하거나 하나도 실행하지 않게) db의 트렌젝션 개념
+atomic<int32> sum = 0;
+void Add() {
+	for (int32 i = 0; i < 100'0000; i++) 
+	{
+		sum.fetch_add(1);
+	}
+}
+void Sub() {
+	for (int32 i = 0; i < 100'0000; i++)
+	{
+		sum.fetch_add(-1);
+	}
+}
+int main()
+{
+	Add();
+	Sub();
+
+	std::thread t1(Add);
+	std::thread t2(Sub);
+
+	t1.join();
+	t2.join();
+	cout << sum << endl;
+}
