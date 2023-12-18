2fa669d7996528ae8eafa8de53eec89aef10f3a9
SangInPark <54829486+tkddls3319@users.noreply.github.com>
2023-12-18 15:58:42 +0900

    2023.12.18
    Reference Counting 및 Shared 포인터

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -7,30 +7,72 @@
 #define OUT
 #include "PlayerManager.h"
 #include "AccountManager.h"
+#include "RefCounting.h"
 
+
+class Wraight : public RefCountable
+{
+public:
+	int _hp = 150;
+	int _posX = 0;
+	int _posY = 0;
+};
+using WraightRef = TSharedPtr<Wraight>;
+class Missile : public RefCountable
+{
+public:
+	void SetTarget(WraightRef target)
+	{
+		_target = target;
+	}
+	bool Update()
+	{
+		if (_target == nullptr)
+			return true;
+
+		int posX = _target->_posX;
+		int posY = _target->_posY;
+
+		//TODO : 쫓아가기
+
+		if (_target->_hp == 0)
+		{
+			_target = nullptr;
+			return true;
+		}
+
+		return false;
+	}
+
+	WraightRef _target = nullptr;
+};
+
+
+using MissileRef = TSharedPtr<Missile>;
 
 int main()
 {
-	GThreadManager->Launch([=]
-		{
-			while (true)
-			{
-				cout << "PlayerThenAccount" << endl;
-				GPlayerManager.PlayerThenAccount();
-				this_thread::sleep_for(100ms);
-			}
-		});
-	GThreadManager->Launch([=]
-		{
-			while (true)
-			{
-				cout << "AccountThenPlayer" << endl;
-				GAccountManager.AccountThenPlayer();
-				this_thread::sleep_for(100ms);
-			}
-		});
+	WraightRef wraight(new Wraight());
+	wraight->ReleaseRef();
+	MissileRef missile(new Missile());
+	missile->ReleaseRef();
 
-	GThreadManager->Join();
+	missile->SetTarget(wraight);
 
+	//레이스 피격 당함
+	wraight->_hp = 0;
+	wraight = nullptr;
+
+	while (true)
+	{
+		if (missile) 
+		{
+			if (missile->Update())
+			{
+				missile = nullptr;
+			}
+		}
+	}
+	wraight = nullptr;
 }
 
