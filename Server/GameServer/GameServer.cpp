973033109b7a410e9abd59b1f243c085b54e0c34
SangInPark <54829486+tkddls3319@users.noreply.github.com>
2024-05-03 09:18:18 +0900

    TypeCast
    TypeCast

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -11,22 +11,58 @@
 #include "Memory.h"
 #include "Allocator.h"
 
-class Knight {
+class Player 
+{
+	Player();
+};
+class Mage :public Player
+{
+public:
+	Mage()
+	{
+		cout << "k" << endl;
+	}
+
+private:
+	int32 _hp = 100;
+};
+
+class Knight :public Player 
+{
 public:
 	Knight()
 	{
 		cout << "k" << endl;
 	}
-	~Knight() {
-
-		cout << "~k" << endl;
+private:
+	int32 _hp = 100;
+};
+class Archer :public Player
+{
+public:
+	Archer()
+	{
+		cout << "k" << endl;
 	}
-
+private:
 	int32 _hp = 100;
 };
 
+template<typename T, typename U>
+struct TypeList
+{
+	using Head = T;
+	using Tail = U;
+};
+
+#include "TypeCast.h"
 int main()
 {
+	TypeList<Mage, Knight>::Head whoAmi;
+	TypeList<Mage, Knight>::Tail whoAmi;
+
+	TypeList<Mage, TypeList<Knight, Archer>>::Tail whoAmi;
+
 	for (int32 i = 0; i < 5; i++)
 	{
 		GthreadManager->Launch([]() {
