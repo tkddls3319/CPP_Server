d6bbe9a451e61d4ac40f17ffd7dd10e776839e3a
SangInPark <54829486+tkddls3319@users.noreply.github.com>
2024-05-23 15:14:14 +0900

    ioctlsocket 사용한 논블로킹 방법

--- Server/DummyClient/DummyClient.cpp
+++ Server/DummyClient/DummyClient.cpp
@@ -20,15 +20,82 @@ int main()
 	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
 		return 0;
 
-	//af : Address Family ( AF_INET = IPv4, AF_INET6 = IPv6
-	//type : TCP(SOCK_STREAM), UDP(SOCK_DGRAM)
-	//protocol : 0
-	//return : 성공여부
-	SOCKET clientSocket = ::socket(AF_INET, SOCK_DGRAM, 0);
+	SOCKET clientSocket = ::socket(AF_INET, SOCK_STREAM, 0);
 	if (clientSocket == INVALID_SOCKET)
-	{
-		HandleError("Socket");
 		return 0;
+
+	//논블로킹을 하기위해
+	u_long on = 1;
+	if (::ioctlsocket(clientSocket, FIONBIO, &on) == INVALID_SOCKET)
+		return 0;
+
+	//나의 주소 : IP주소 + PORT
+	SOCKADDR_IN serverAddr;//IPv4
+	::memset(&serverAddr, 0, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	::inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
+	serverAddr.sin_port = ::htons(7777);
+
+
+	//Connect
+	while (true)
+	{
+		if (::connect(clientSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) 
+		{
+			//원래 블록했어야함.
+			if (::WSAGetLastError() == WSAEWOULDBLOCK)
+				continue;
+
+			//이미 연결된 상태라면 break;
+			if (::WSAGetLastError() == WSAEISCONN)
+				break;
+
+			//ERROR
+			break;
+		}
+	}
+	cout << "Server Connected!" << endl;
+
+	char sendBuffer[100] = "Hello World";
+
+	while (true)
+	{
+		if (::send(clientSocket, sendBuffer, sizeof(sendBuffer), 0) == SOCKET_ERROR)
+		{
+			//원래 블록했어야함.
+			if (::WSAGetLastError() == WSAEWOULDBLOCK)
+				continue;
+
+			//Error
+			break;
+		}	
+		cout << "send Data Len" << sizeof(sendBuffer) << endl;
+	
+
+		while (true)
+		{
+			char recvBuffer[1000];
+			int32 recvLen = ::recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0);
+			if (recvLen == SOCKET_ERROR)
+			{
+				//원래 블록했어야함.
+				if (::WSAGetLastError() == WSAEWOULDBLOCK)
+					continue;
+
+				//Error
+				break;
+			}
+			else if (recvLen == 0)
+			{
+				//연결끊김
+				break;
+			}
+		
+			cout << "Recv Data Len = " << recvLen << endl;
+			break;;
+		}
+	
+		this_thread::sleep_for(1s);
 	}
 
 	//소켓 리소스 반환
--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -26,61 +26,89 @@ int main()
 	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
 		return 0;
 
-	SOCKET serverSocket = ::socket(AF_INET, SOCK_DGRAM, 0);
+	SOCKET listenSocket = ::socket(AF_INET, SOCK_STREAM, 0);
+	if (listenSocket == INVALID_SOCKET)
+		return 0;
 
-	if (serverSocket == INVALID_SOCKET)
+	//논블로킹을 하기위해
+	u_long on = 1;
+	if (::ioctlsocket(listenSocket, FIONBIO, &on) == INVALID_SOCKET)
+		return 0;
+
+	//나의 주소 : IP주소 + PORT
+	SOCKADDR_IN serverAddr;//IPv4
+	::memset(&serverAddr, 0, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_addr.s_addr = ::htonl(INADDR_ANY);//아이피 알아서 설정
+	serverAddr.sin_port = ::htons(7777);//PORT
+
+	//socket과 주소 연동
+	if (::bind(listenSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+		return 0;
+
+	//서버오픈
+	if (::listen(listenSocket, 10) == SOCKET_ERROR)//10은 대기열 이걸넘으면 못들어옴
+		return 0;
+
+	cout << "Accept" << endl;
+
+	SOCKADDR_IN clientAddr;//IPv4
+	::memset(&clientAddr, 0, sizeof(clientAddr));
+	int32 addrLen = sizeof(clientAddr);
+
+
+	while (true)
 	{
-		HandleError("Socket");
+		SOCKET clientSocket = ::accept(listenSocket, (SOCKADDR*)&clientAddr, &addrLen);
+		if (clientSocket == INVALID_SOCKET)
+		{
+			//원래 블록했어야함.
+			if (::WSAGetLastError() == WSAEWOULDBLOCK)
+				continue;
+
+			//Error
+			break;
+		}
+		cout << "Client Connected!" << endl;
+
+		while (true)
+		{
+			char recvBuffer[1000];
+			int32 recvLen = ::recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0);
+			if (recvLen == SOCKET_ERROR)
+			{
+				//원래 블록했어야함.
+				if (::WSAGetLastError() == WSAEWOULDBLOCK)
+					continue;
+
+				//Error
+				break;
+			}
+			else if (recvLen == 0)
+			{
+				//연결끊김
+				break;
+			}
+
+			cout << "Recv Data Len = " << recvLen << endl;
+
+			while (true)
+			{
+				if (::send(clientSocket, recvBuffer, recvLen, 0) == SOCKET_ERROR)
+				{
+					//원래 블록했어야함.
+					if (::WSAGetLastError() == WSAEWOULDBLOCK)
+						continue;
+
+					//Error
+					break;
+				}
+
+				cout << "send Data Len" << recvLen<< endl;
+				break;
+			}
+		}
 	}
 
-	//SO_KEEPALIVE = 주기적으로 연결 상태 확인 여부 (TCP Only), 상대방이 갑자기 연결을 끊는다면 주기적으로 tcp 프로토콜 연결 상태확인
-	bool enable = true;
-	::setsockopt(serverSocket, SOL_SOCKET, SO_KEEPALIVE, (char*)&enable, sizeof(enable));
-
-	//SO_LINGER =  지연하다. send도중 closesocket를 하여 소켓 리소스 반환을 한다면 송신 버퍼에 있는 데이터를 보낼것인가 날릴것인가
-	//l_oneff = 0이면 closesocket()이 바로 리턴 아니면 linger 초만큼 대기 (default 0)
-	//l_linger = 대기시간
-	LINGER linger;
-	linger.l_onoff = 1;
-	linger.l_linger = 5;
-	::setsockopt(serverSocket, SOL_SOCKET, SO_LINGER, (char*)&linger, sizeof(linger));
-
-	//Half-Close
-	//SD_SEND : send 만 막는다
-	//SD_RECEIVE : recv 만 막는다.
-	//SD_BOATH : 둘다 막는다.
-	//::shutdown(serverSocket, SD_SEND);
-	//소켓 리소스 반환, SEND하는 도중에 닫아버릴 수 있다. SHUTDOWND과 함께 나와줘야 좋음.
-	//::closesocket(serverSocket);
-
-	//SO_SNDBUF = 송신 버퍼 크기
-	//SO_RCVBUF = 수신 버퍼 크기
-	int32 sendBufferSize;
-	int32 optionLen = sizeof(sendBufferSize);
-	::getsockopt(serverSocket, SOL_SOCKET, SO_SNDBUF, (char*)&sendBufferSize, &optionLen);
-	cout << "송신 버퍼 크기 : " << sendBufferSize << endl;
-	int32 recvBufferSize;
-	 optionLen = sizeof(recvBufferSize);
-	::getsockopt(serverSocket, SOL_SOCKET, SO_RCVBUF, (char*)&recvBufferSize, &optionLen);
-	cout << "수신 버퍼 크기 : " << recvBufferSize << endl;
-
-	//SO_REUSEADDR
-	//IP주소 및 PORT 재사용 - 서버 껐다켰을 때 재사용 설정안해주면 3분정도 뒤에 다시 사용가능하기 때문에 설정해줘야함.
-	{
-		bool enable = true;
-		::setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, (char*)&enable, sizeof(enable));
-	}
-
-	//IPPROTO_TCP
-	//TCP_NODELAY = Nagle 네이글 알고리즘 작동을 끈다.
-	//데이터가 충분히 크면 보내고 그렇지 않으면 데이터가 충분히 쌓일때까지 대기.
-	//장점 : 작은 패킷이 불필요하게 많이 생성되는 일 방지
-	//단점 : 반응 시간 손해
-	{
-		bool enable = true;
-		::setsockopt(serverSocket, IPPROTO_TCP, TCP_NODELAY, (char*)&enable, sizeof(enable));
-	}
-
-	//윈도우 소켓 종료 WSADATA호출 수만큼 정리해줘야함(권고사항)
 	::WSACleanup();
 }
