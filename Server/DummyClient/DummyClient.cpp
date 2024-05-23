e0a0f654ab84d0ea97c9ce2227f45f58fca70c0f
SangInPark <54829486+tkddls3319@users.noreply.github.com>
2024-05-23 10:00:19 +0900

    소켓생성 및 send recv 기본 생성 (블로킹방식)

--- Server/DummyClient/DummyClient.cpp
+++ Server/DummyClient/DummyClient.cpp
@@ -1,7 +1,85 @@
 ﻿#include "pch.h"
 #include <iostream>
 #include "CorePch.h"
+
+#include <WinSock2.h>
+#include <mswsock.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+
 int main()
 {
+	//윈소켓 초기화(w2_32 라이브러리 초기화)
+	//관련정보가 wsaData에 채워짐
+	WSADATA wsaData;
+	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		return 0;
 
+	//af : Address Family ( AF_INET = IPv4, AF_INET6 = IPv6
+	//type : TCP(SOCK_STREAM), UDP(SOCK_DGRAM)
+	//protocol : 0
+	//return : 성공여부
+	SOCKET clientSocket = ::socket(AF_INET, SOCK_STREAM, 0);
+	if (clientSocket == INVALID_SOCKET)
+	{
+		int32 errCode = ::WSAGetLastError();
+		cout << "socket error code" << errCode << endl;
+		return 0;
+	}
+
+	//연결할 목적지 : IP주소 + PORT
+	SOCKADDR_IN serverAddr;//IPv4
+	::memset(&serverAddr, 0, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	//serverAddr.sin_addr.s_addr = ::inet_addr("127.0.0.1");
+	::inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
+	serverAddr.sin_port = ::htons(7777);//PORT
+	//htons(host to network short) Little-Endian, Big-Endian
+
+	//서버에 연결
+	if (::connect(clientSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+	{
+		int32 errCode = ::WSAGetLastError();
+		cout << "Connect error code" << errCode << endl;
+		return 0;
+	}
+
+	//--------------------------
+	///연결성공
+
+	cout << "Connected To Server!" << endl;
+
+	while (true)
+	{
+		char sendBuffer[100] = "Hellow World!";
+		int32 resultCode = ::send(clientSocket, sendBuffer, sizeof(sendBuffer), 0);
+		if (resultCode == SOCKET_ERROR)
+		{
+			int32 errCode = ::WSAGetLastError();
+			cout << "Send error code" << errCode << endl;
+			return 0;
+		}
+
+		cout << "Send Data Len = " << sizeof(sendBuffer) << endl;
+
+		char recvBuffer[1000];
+		int32 recvLen = ::recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0);
+
+		if (recvLen <= 0)
+		{
+			int32 errCode = ::WSAGetLastError();
+			cout << "Recv error code" << errCode << endl;
+			return 0;
+		}
+		cout << "Recv Data = " << recvBuffer << endl;
+
+		this_thread::sleep_for(1s);
+	}
+	//--------------------------
+
+	//소켓 리소스 반환
+	::closesocket(clientSocket);
+
+	//윈도우 소켓 종료 WSADATA호출 수만큼 정리해줘야함(권고사항)
+	::WSACleanup();
 }
--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -7,76 +7,102 @@
 #include <future>
 #include "ThreadManager.h"
 
-#include "RefCounting.h"
-#include "Memory.h"
-#include "Allocator.h"
+#include <WinSock2.h>
+#include <mswsock.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
 
-class Player 
-{
-	Player();
-};
-class Mage :public Player
-{
-public:
-	Mage()
-	{
-		cout << "k" << endl;
-	}
-
-private:
-	int32 _hp = 100;
-};
-
-class Knight :public Player 
-{
-public:
-	Knight()
-	{
-		cout << "k" << endl;
-	}
-private:
-	int32 _hp = 100;
-};
-class Archer :public Player
-{
-public:
-	Archer()
-	{
-		cout << "k" << endl;
-	}
-private:
-	int32 _hp = 100;
-};
-
-template<typename T, typename U>
-struct TypeList
-{
-	using Head = T;
-	using Tail = U;
-};
-
-#include "TypeCast.h"
 int main()
 {
-	TypeList<Mage, Knight>::Head whoAmi;
-	TypeList<Mage, Knight>::Tail whoAmi;
+	//윈소켓 초기화(w2_32 라이브러리 초기화)
+	//관련정보가 wsaData에 채워짐
+	WSADATA wsaData;
+	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		return 0;
 
-	TypeList<Mage, TypeList<Knight, Archer>>::Tail whoAmi;
-
-	for (int32 i = 0; i < 5; i++)
+	//af : Address Family ( AF_INET = IPv4, AF_INET6 = IPv6
+	//type : TCP(SOCK_STREAM), UDP(SOCK_DGRAM)
+	//protocol : 0
+	//return : 성공여부
+	SOCKET listenSocket = ::socket(AF_INET, SOCK_STREAM, 0);
+	if (listenSocket == INVALID_SOCKET)
 	{
-		GthreadManager->Launch([]() {
-
-			while (true)
-			{
-				Vector<Knight> v(10);
-				Map<int32, Knight> m;
-				m[100] = Knight();
-
-				this_thread::sleep_for(10ms);
-			}
-			});
+		int32 errCode = ::WSAGetLastError();
+		cout << "socket error code" << errCode << endl;
+		return 0;
 	}
 
-	GthreadManager->Join();
+	//나의 주소 : IP주소 + PORT
+	SOCKADDR_IN serverAddr;//IPv4
+	::memset(&serverAddr, 0, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_addr.s_addr = ::htonl(INADDR_ANY);//아이피 알아서 설정
+	serverAddr.sin_port = ::htons(7777);//PORT
+	//htons(host to network short) Little-Endian, Big-Endian
+
+	//socket과 주소 연동
+	if (::bind(listenSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)))
+	{
+		int32 errCode = ::WSAGetLastError();
+		cout << "Bind error code" << errCode << endl;
+		return 0;
+	}
+
+	//서버오픈
+	if (::listen(listenSocket, 10) == SOCKET_ERROR)//10은 대기열 이걸넘으면 못들어옴
+	{
+		int32 errCode = ::WSAGetLastError();
+		cout << "Listen error code" << errCode << endl;
+		return 0;
+	}
+
+	//--------------------------
+	///오픈성공
+	while (true)
+	{
+		SOCKADDR_IN clientAddr;//IPv4
+		::memset(&clientAddr, 0, sizeof(clientAddr));
+		int32 addrLen = sizeof(clientAddr);
+		SOCKET clientSocket = ::accept(listenSocket, (SOCKADDR*)&clientAddr, &addrLen);
+
+		if (clientSocket == INVALID_SOCKET)
+		{
+			int32 errCode = ::WSAGetLastError();
+			cout << "Accept error code" << errCode << endl;
+			return 0;
+		}
+
+		//클라이언트 입장
+		char ipAddress[16];
+		::inet_ntop(AF_INET, &clientAddr.sin_addr, ipAddress, sizeof(ipAddress));
+
+		cout << "Client Connected! IP = " << ipAddress << endl;
+	
+		while (true)
+		{
+			char recvBuffer[1000];
+			int32 recvLen = ::recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0);
+
+			if (recvLen <= 0)
+			{
+				int32 errCode = ::WSAGetLastError();
+				cout << "Recv error code" << errCode << endl;
+				return 0;
+			}
+
+			cout << "Recv Data = " << recvBuffer << endl;
+
+			int32 resultCode = ::send(clientSocket, recvBuffer, recvLen, 0);
+			if (resultCode == SOCKET_ERROR)
+			{
+				int32 errCode = ::WSAGetLastError();
+				cout << "Send error code" << errCode << endl;
+				return 0;
+			}
+		}
+	}
+	//--------------------------
+
+	//윈도우 소켓 종료 WSADATA호출 수만큼 정리해줘야함(권고사항)
+	::WSACleanup();
 }
